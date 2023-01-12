184f0d54efe377300fd77fc99e367f033bcb98d2
Gustavo Ariel Leguizamon <gustavo.ariel.leguizamon@gmail.com>
2023-01-12 00:12:01 -0300

    formularios de logueo

--- proyecto/base/views.py
+++ proyecto/base/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from django.urls import reverse_lazy
 from .models import Tarea
+
+
+class Logueo(LoginView):
+    template_name = "base/login.html"
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tareas')
 
 class ListaPendientes(ListView):
     model = Tarea
@@ -14,3 +26,18 @@ class DetalleTarea(DetailView):
     context_object_name = 'tarea'
     template_name = 'base/tarea.html'
 
+class CrearTarea(CreateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
+
+
+class EditarTarea(UpdateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
+
+class BorrarTarea(DeleteView):
+    model = Tarea
+    context_object_name = 'tareas'
+    success_url = reverse_lazy('tareas')
